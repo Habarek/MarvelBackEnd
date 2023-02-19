d73b7071e95f321e0d6124fcd54e8141d93f77a5
Ali <ali@MacBook-Pro-de-Ali.local>
2023-02-19 23:26:51 +0100

    marvelBack

--- index.js
+++ index.js
@@ -9,18 +9,22 @@ const cors = require("cors");
 app.use(express.json());
 app.use(cors());
 
-// app est le server, .get et le type de requête /comics est la route de mon front faire un async pour attendre la reponse
+// app est le server, .get et le type de requête /comics est la route de mon front faire un async pour attendre la reponse.
 //  (req,res) sont les requête et les réponses
 app.get("/comics", async (req, res) => {
   try {
     console.log(req.query.title);
+
+    // création de la variable title pour faire les recherche sur la search-bar
+    const title = req.query.title;
     // response est ma variable qui va stocker les donnée de la requête
     // await permet d'attendre la requête axios .get est le type de requête
     const response = await axios.get(
       // L'adresse de l'API MARVEL
-      `https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=${process.env.API_KEY}`
+      `https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=${process.env.API_KEY}&title=${title}`
     );
     console.log(response.data);
+
     // la réponse que sera envoyer à mon front
     res.json(response.data);
   } catch (error) {
@@ -30,10 +34,13 @@ app.get("/comics", async (req, res) => {
 
 app.get("/characters", async (req, res) => {
   try {
-    console.log(req.query.title);
+    console.log(req.query.name);
 
+    // création de variable name qui contient la requête de la barre de cherche
+    // sinon on peur écrire 'req.query.name' apres l'API_KEY
+    const name = req.query.name;
     const response = await axios.get(
-      `https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${process.env.API_KEY}`
+      `https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${process.env.API_KEY}&name=${name}`
     );
     // console.log(response.data);
     res.json(response.data);
@@ -42,10 +49,24 @@ app.get("/characters", async (req, res) => {
   }
 });
 
-//
+app.get("/information/:id", async (req, res) => {
+  try {
+    console.log(req.query._id);
+    let id = req.query._id;
+    // création de variable name qui contient la requête de la barre de cherche
+    // sinon on peur écrire 'req.query.name' apres l'API_KEY
+
+    const response =
+      await axios.get// `https://lereacteur-marvel-api.herokuapp.com/character/id=${id}?apiKey=${process.env.API_KEY}`; // `https://lereacteur-marvel-api.herokuapp.com/character/5fcf91f4d8a2480017b91453?apiKey=${process.env.API_KEY}` // `https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${process.env.API_KEY}&name=${name}&information/id=${id}` // `https://lereacteur-marvel-api.herokuapp.com/character/5fcf91f4d8a2480017b91453?apiKey=${process.env.API_KEY}&information/:id=${id}`
+      // `https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${process.env.API_KEY}&information/:id=${id}` X
+      `https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${process.env.API_KEY}`;
+    // console.log(response.data);
+    res.json(response.data);
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
 app.listen(process.env.PORT, () => {
   console.log("Server started🚀");
 });
-
-// "https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=zt5kC3pABOo2Qlt4"
-// zt5kC3pABOo2Qlt4
